0665ec5ac187aa41835e3993d73f6629f5f29db3
Joseph Martin <jsmartin@softlayer.com>
2013-06-23 17:11:22 -0500

    Add Test Kitchen support and re-factor to use the python cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'klandreth@softlayer.com'
 license          'MIT'
 description      'Installs/Configures swftp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
+
+depends          "python"
